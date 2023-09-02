d072b328c060b9e96e0834ebfc016dd31c48d817
Rafael <bboyrafinhazika@gmail.com>
2023-09-02 18:03:20 -0300

    commit changes

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -133,7 +133,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none  aria-selected:bg-primary/80 aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none  aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       className,
     )}
     {...props}
