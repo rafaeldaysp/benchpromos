2ec26be4fe091a66c0c3caa84f13b530d465fc7c
Rafael <bboyrafinhazika@gmail.com>
2023-09-14 01:09:42 -0300

    fix commit

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,19 +1,12 @@
-import { headers } from 'next/headers'
-import { userAgent } from 'next/server'
-
 import { getCurrentUser } from '@/app/_actions/user'
 import { Sales } from '@/components/sales/sales'
 
 export default async function Home() {
-  const headersList = headers()
   const user = await getCurrentUser()
-
-  const { device } = userAgent({ headers: headersList })
-  const viewport = device.type === 'mobile' ? 'mobile' : 'desktop'
 
   return (
     <div className="px-4 sm:container">
-      <Sales user={user} viewport={viewport} />
+      <Sales user={user} />
     </div>
   )
 }
