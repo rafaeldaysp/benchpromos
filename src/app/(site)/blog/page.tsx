ef7ecec13f173238d52e193fc6f557a86c9fc5e5
Rafael <bboyrafinhazika@gmail.com>
2024-02-15 15:50:54 -0300

    remove dev stuffs

--- src/app/(site)/blog/page.tsx
+++ src/app/(site)/blog/page.tsx
@@ -93,11 +93,6 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
           })}
         </div>
       </main>
-
-      <span>
-        {JSON.stringify(pagesResponse)}
-        {category}
-      </span>
     </div>
   )
 }
