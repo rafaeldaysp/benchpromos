26f31e3974bceb09ce1f21c5264222cde8b5514c
Rafael <86262597+rafaeldaysp@users.noreply.github.com>
2024-07-21 17:12:22 -0300

    add dashboard page (#9)

---
+++ src/components/dashboard/sales-by-reactions.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import Link from 'next/link'
+import { useState } from 'react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { type Sale } from '@/types'
+import { Icons } from '../icons'
+import { Button } from '../ui/button'
+import { ScrollArea } from '../ui/scroll-area'
+
+interface SalesByReactionsProps {
+  data: {
+    sale: Sale
+    _count: {
+      reactions: number
+      comments: number
+    }
+  }[]
+}
+
+export function SalesByReactions({ data }: SalesByReactionsProps) {
+  const [currentPage, setCurrentPage] = useState(1)
+  const pages = Math.ceil(data.length / 5)
+  const paginatedData = data.slice(currentPage - 1, currentPage + 5)
+  return (
+    <Card id="#salesByReaction">
+      <CardHeader>
+        <CardTitle>Promoções em destaque</CardTitle>
+        <CardDescription>
+          Página {currentPage} de {pages}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="px-0">
+        <ScrollArea className="h-[320px]">
+          <div className="space-y-8 px-6">
+            {paginatedData.map((saleData) => (
+              <div key={saleData.sale.id} className="flex items-center">
+                <Avatar className="h-9 w-9">
+                  <AvatarImage src={saleData.sale.imageUrl} alt="Avatar" />
+                  <AvatarFallback>N/A</AvatarFallback>
+                </Avatar>
+                <div className="ml-4 space-y-1">
+                  <Link
+                    href={`/promocao/${saleData.sale.slug}/${saleData.sale.id}`}
+                    className="text-sm transition-colors hover:text-primary"
+                    target="_blank"
+                  >
+                    {saleData.sale.title}
+                  </Link>
+                  <p className="text-sm text-muted-foreground">
+                    {saleData._count.reactions} reações e{' '}
+                    {saleData._count.comments} comentários
+                  </p>
+                </div>
+                <div className="ml-auto font-medium">
+                  +{saleData._count.comments + saleData._count.reactions}
+                </div>
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-center justify-center">
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+            disabled={currentPage == 1}
+            onClick={() => setCurrentPage(1)}
+          >
+            <Icons.ChevronsLeft className="h-4 w-4" />
+          </Button>
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+            disabled={currentPage == 1}
+            onClick={() => setCurrentPage((v) => v - 1)}
+          >
+            <Icons.ChevronLeft className="h-4 w-4" />
+          </Button>
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+            disabled={currentPage == pages}
+            onClick={() => setCurrentPage((v) => v + 1)}
+          >
+            <Icons.ChevronRight className="h-4 w-4" />
+          </Button>
+          <Button
+            variant={'ghost'}
+            size={'icon'}
+            disabled={currentPage == pages}
+            onClick={() => setCurrentPage(pages)}
+          >
+            <Icons.ChevronsRight className="h-4 w-4" />
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
---
+++ src/components/layouts/dashboard-nav.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+import { cn } from '@/lib/utils'
+
+const sidebarNavItems = [
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+  },
+  {
+    title: 'Promoções',
+    href: '/dashboard/sales',
+  },
+  {
+    title: 'Produtos',
+    href: '/dashboard/products',
+  },
+  {
+    title: 'Ofertas',
+    href: '/dashboard/deals',
+  },
+  {
+    title: 'Varejistas',
+    href: '/dashboard/retailers',
+  },
+  {
+    title: 'Categorias',
+    href: '/dashboard/categories',
+  },
+  {
+    title: 'Cupons',
+    href: '/dashboard/coupons',
+  },
+  {
+    title: 'Cashbacks',
+    href: '/dashboard/cashbacks',
+  },
+  {
+    title: 'Usuários',
+    href: '/dashboard/users',
+  },
+  {
+    title: 'Benchmarks',
+    href: '/dashboard/benchmarks',
+  },
+]
+
+interface DashboardNavProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function DashboardNav({ className, ...props }: DashboardNavProps) {
+  const pathname = usePathname()
+
+  return (
+    <div className="relative">
+      <ScrollArea className="max-w-[600px] lg:max-w-none">
+        <div className={cn('mb-4 flex items-center', className)} {...props}>
+          {sidebarNavItems.map((item, index) => (
+            <Link
+              href={item.href}
+              key={item.href}
+              className={cn(
+                'flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary',
+                pathname === item.href || (index === 0 && pathname === '/')
+                  ? 'bg-muted font-medium text-primary'
+                  : 'text-muted-foreground',
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" className="invisible" />
+      </ScrollArea>
+    </div>
+  )
+}
