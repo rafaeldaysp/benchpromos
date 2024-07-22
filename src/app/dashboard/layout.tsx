717e59bb87dce42cdf2421b6aceb72f440be89ed
rafaeldaysp <bboyrafinhazika@gmail.com>
2024-07-22 16:17:42 -0300

    add home button on dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link'
 import * as React from 'react'
 
+import { Icons } from '@/components/icons'
 import { DashboardNav } from '@/components/layouts/dashboard-nav'
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -8,11 +12,19 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col space-y-8 px-4 py-10 sm:container lg:space-y-0">
-      <aside className="overflow-x-auto">
-        <DashboardNav />
-      </aside>
-      <div className="flex-1">{children}</div>
+    <div className="min-h-screen space-y-6 px-4 py-10 sm:container">
+      <Link href="/" className={cn(buttonVariants({ variant: 'ghost' }), '')}>
+        <>
+          <Icons.ChevronLeft className="mr-2 h-4 w-4" />
+          Início
+        </>
+      </Link>
+      <div className="flex flex-col gap-6">
+        <aside className="overflow-x-auto">
+          <DashboardNav />
+        </aside>
+        <div className="flex-1">{children}</div>
+      </div>
     </div>
   )
 }
