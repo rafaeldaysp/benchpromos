4aebd8a3abdde0663ba3f20d72ce430b719db235
rafael <bboyrafinhazika@gmail.com>
2025-08-21 16:02:50 -0300

    add http config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: 'minio.benchpromos.com.br',
+      },
     ],
   },
   experimental: {
